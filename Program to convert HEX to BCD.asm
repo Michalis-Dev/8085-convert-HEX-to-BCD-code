cfe77bb325d4827577caee35dfc2edb6877f3156
MICHALIS KITSAKIS <68616483+Michalis-Dev@users.noreply.github.com>
2022-02-27 20:15:33 +0200

    New Program asm
    Program for 8085 convert  HEX to BCD

---
+++ Program to convert HEX to BCD.asm
@@ -0,0 +1,32 @@
+
+;<Program title>
+
+jmp start
+
+;data
+
+
+;code
+start: nop
+LXI H,8000H	; initialize memory point
+MVI D,00H	; clear D - reg for most significant byte
+XRA A		; clear accumulator
+MOV C,M		; get hex data
+LOOP: ADI 01H	; count the number one by one
+DAA		; adjust for BCD count
+JNC SKIP	; jump to skip
+INR D		; increase D
+SKIP: DCR C	; decrease C register
+JNZ LOOP 	; jump to loop
+MOV L,A		; load the last significant byte
+MOV H,D		; load the most Significant byte
+
+RRC
+RRC
+RRC
+RRC
+MOV B,A
+
+
+SHLD 8050H	; store the BCD
+HLT		; terminate the program
